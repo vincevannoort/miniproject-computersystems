50364798b5ab44cad196789999317fea2504f1ca
Vince van Noort <vince.vannoort@student.hu.nl>
2016-10-31 14:40:36 +0100

    Initalisatie Firebase

--- applicatie/inbraak-alarm.py
+++ applicatie/inbraak-alarm.py
@@ -1,1 +1,10 @@
-print('Hitler <3')
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyDse4S8imxqiaDRELr58a8P45r7G5Af2go",
+  "authDomain": "miniproject-computersystems.firebaseapp.com",
+  "databaseURL": "https://miniproject-computersystems.firebaseio.com",
+  "storageBucket": "miniproject-computersystems.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
