84b5a088349cdc56d73d1862514b1df00d6e1206
Vince van Noort <vince.vannoort@student.hu.nl>
2016-10-31 17:20:54 +0100

    Basis Inbraak Alarm

--- applicatie/inbraak-alarm.py
+++ applicatie/inbraak-alarm.py
@@ -1,1 +1,55 @@
-print('Hitler <3')
+import pyrebase
+import datetime
+import pygame
+
+# Configuratie voor de firebase database
+config = {
+  'apiKey': 'AIzaSyDse4S8imxqiaDRELr58a8P45r7G5Af2go',
+  'authDomain': 'miniproject-computersystems.firebaseapp.com',
+  'databaseURL': 'https://miniproject-computersystems.firebaseio.com',
+  'storageBucket': 'miniproject-computersystems.appspot.com',
+  'serviceAccount': 'database/miniproject-computersystems-c79e0f2b03d3.json'
+}
+
+# Initaliseer applicatie via pyrebase & PyGame
+firebase = pyrebase.initialize_app(config)
+pygame.init()
+
+# Authenticeer applicatie
+auth = firebase.auth()
+
+# Firebase database connectie
+db = firebase.database()
+
+# Class inbraakalarm
+class InbraakAlarm():
+
+    # Initalisatie inbraakalarm
+    # Statussen -> idle, armed, unarmed, settings
+    def __init__(self):
+        self._status = 'idle'
+
+    # Haal de status op
+    def get_status(self):
+        return self._status
+
+    # Wijzig de status
+    def set_status(self, status):
+        self._status = status
+        db.child('logs').push({'status': self.get_status(), 'timestamp': str(datetime.datetime.now())})
+        print('status wijziging')
+
+    global_status = property(get_status, set_status)
+
+# Begin de applicatie
+if __name__ == '__main__':
+    app = InbraakAlarm()
+
+    # Laat de applicatie runnen
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    app.global_status = 'armed'
+                if event.key == pygame.K_RIGHT:
+                    app.global_status = 'unarmed'
---
+++ applicatie/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pyrebase',
+    version='3.0.18',
+    url='https://github.com/thisbejim/Pyrebase',
+    description='A simple python wrapper for the Firebase API',
+    author='James Childs-Maidment',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='Firebase',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'requests', 'sseclient', 'gcloud',
+        'oauth2client', 'requests_toolbelt', 'python_jwt',
+        'pycrypto'
+    ]
+)
