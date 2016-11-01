15b16eface9c8b99cbfb8d07283db4b709e18b83
Vince van Noort <vince.vannoort@student.hu.nl>
2016-11-01 18:52:28 +0100

    Statussen verder verwerkt in het systeem, functionaliteit settings wijzigen toegevoegd.

--- applicatie/inbraak-alarm.py
+++ applicatie/inbraak-alarm.py
@@ -1,6 +1,8 @@
 import sqlite3
 import datetime
 import pygame
+# import RPi.GPIO as GPIO -> MOET AANWORDEN GEZET
+from threading import Timer
 
 # Initaliseer applicatie via PyGame
 pygame.init()
@@ -20,8 +22,10 @@ class InbraakAlarm():
         self._status = 'unarmed'
         self._previousstatus = 'unarmed'
         self._beveiligingscode = '1111'
-        self._userbeveiliginscode = ''
+        self._userbeveiligingscode = ''
         self._deurdicht = True
+        self._timer = Timer(3.0, self.start_anti_inbraak)
+        self._closesettings = 0
 
     # Haal de status op
     def get_status(self):
@@ -31,25 +35,42 @@ class InbraakAlarm():
     def set_status(self, status):
         self._previousstatus = self._status
         self._status = status
-        print('STATUS: {}'.format(self._status))
+        print('Status gewijzigd: {}'.format(self._status))
         c.execute("INSERT INTO logs (status) VALUES ('{}')".format(self.get_status()))
+
+        # Wijzig led status wanneer status wordt veranderd
+        if status == 'unarmed':
+            print('-> WIJZIG LEDS HIER <-')
+        elif status == 'armed':
+            print('-> WIJZIG LEDS HIER <-')
+        elif status == 'inputcode':
+            print('-> WIJZIG LEDS HIER <-')
+        elif status == 'lockdown':
+            print('-> WIJZIG LEDS HIER <-')
+        elif status == 'settings':
+            print('-> WIJZIG LEDS HIER <-')
 
     global_status = property(get_status, set_status)
 
-    # Haal de status op
+    # Haal de deurstatus op
     def get_deurstatus(self):
         return self._deurdicht
 
-    # Haal de status op
+    # Wijzig de deurstatus
     def set_deurstatus(self, deurstatus):
+        if deurstatus:
+            print('De deur wordt \033[31mdichtgedaan\033[30m.')
+        else:
+            print('De deur wordt \033[32mopengedaan\033[30m.')
         self._deurdicht = deurstatus
-        print('Deur dicht is: {}'.format(app._deurdicht))
         if self._status == 'armed' and self._deurdicht == False:
-            print('AHHHHH ER WORDT INGEBROKEN :OOOOOO')
             app.global_status = 'lockdown'
+            self._timer.start()
 
-    def start_anti_inbraak():
-        pass
+    # Check of de status is veranderd
+    def start_anti_inbraak(self):
+        print('GET OUT, INITIATE \033[91mLOCKDOWN!\033[30m CALL 911!')
+        # print('INBREKER, INITIEER LOCKDOWN NU!')
 
     global_deurstatus = property(get_deurstatus, set_deurstatus)
 
@@ -60,12 +81,25 @@ if __name__ == '__main__':
     # Laat de applicatie runnen
     while True:
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit();
                 conn.commit()
                 conn.close()
 
             if event.type == pygame.KEYDOWN:
+
+                # Verander de status van de deur door D in te drukken
+                if event.key == pygame.K_KP_PLUS:
+                    app.global_deurstatus = (not app.global_deurstatus)
+
+                # Huidige status van het inbraaksysteem
+                if event.key == pygame.K_KP_MINUS:
+                    print(app._status)
+
+                # Werkt alleen wanneer je de settingsknop indrukt als de status unarmed is
+                if event.key == pygame.K_KP_PERIOD and app.global_status == 'unarmed':
+                    app.global_status = 'settings'
 
                 # Werkt alleen als status unarmed is of idle
                 if app.global_status == 'unarmed':
@@ -80,41 +114,6 @@ if __name__ == '__main__':
                     if event.key == pygame.K_KP0:
                         app.global_status = 'inputcode'
                         print('Voer beveiliginscode in: ')
-                
-                # Verander de status van de deur door D in te drukken
-                if event.key == pygame.K_KP_PERIOD:
-                    app.global_deurstatus = (not app.global_deurstatus)
-
-                # Huidige status van het inbraaksysteem
-                if event.key == pygame.K_KP_PLUS:
-                    print(app._status)
-
-                # Werkt alleen als er een code ingevoerd moet worden
-                if app.global_status == 'inputcode':
-                    if event.key and event.key != pygame.K_KP0:
-
-                        # Alleen nieuwe code cijfers toevoegen indien de vorige code kleiner is dan 4
-                        if len(app._userbeveiliginscode) < 4:
-                            app._userbeveiliginscode += pygame.key.name(event.key)[1:-1]
-                            print('Nog {} in te voeren.'.format(4 - len(app._userbeveiliginscode)), end='\r')
-
-                        # Als de code gelijk is aan 4 cijfers, dan check je of de code juist is ingevoerd
-                        if len(app._userbeveiliginscode) == 4:
-                            if app._beveiligingscode == app._userbeveiliginscode:
-                                print('De code is juist ingevoerd, de status wordt ingesteld naar armed')
-                                if app._previousstatus == 'unarmed':
-                                    if app._deurdicht == True:
-                                        app.global_status = 'armed'
-                                    else:
-                                        app.global_status = app._previousstatus
-                                        print('De deur is nog open, deze dient eerst dichtgedaan te worden voordat het alarm kan worden ingesteld.')
-                                elif app._previousstatus == 'armed' or app._previousstatus == 'burglary':
-                                    app.global_status = 'unarmed'
-                                app._userbeveiliginscode = ''
-                            else:
-                                print('De code is onjuist ingevoerd, de status wordt ingesteld naar {}'.format(app._previousstatus))
-                                app.global_status = app._previousstatus
-                                app._userbeveiliginscode = ''
 
                 # Als het alarm is geactiveerd voor inbraak
                 if app.global_status == 'lockdown':
@@ -122,3 +121,64 @@ if __name__ == '__main__':
                     if event.key == pygame.K_KP0:
                         app.global_status = 'inputcode'
                         print('Voer beveiliginscode in: ')
+
+                # Werkt alleen als de settings worden veranderd
+                if app.global_status == 'settings':
+                    # Return als er weer op het puntje wordt gedrukt
+                    if event.key == pygame.K_KP_PERIOD:
+                        if app._closesettings == 0: 
+                            app._closesettings+= 1
+                        elif app._closesettings == 1: 
+                            app.global_status = app._previousstatus
+                            app._closesettings = 0
+
+                    # Indien de key 0 wordt ingedrukt, kun je een 4 cijferige code invoeren
+                    if event.key == pygame.K_KP0:
+                        app.global_status = 'settings-check' # set previous state
+                        app.global_status = 'inputcode'
+                        print('Voer de oude beveiliginscode in om een nieuwe in te stellen: ')
+
+                    # Indien de key 1 wordt ingedrukt, wordt er algemene info getoond
+                    if event.key == pygame.K_KP1:
+                        print('Verander code via sms indien nodig (als je het bent vergeten.')
+
+                # Wordt voor meerdere doeleinden gebruikt qua codeinvoeren
+                if app.global_status == 'inputcode':
+                    if event.key and event.key != pygame.K_KP0:
+
+                        # Alleen nieuwe code cijfers toevoegen indien de vorige code kleiner is dan 4
+                        if len(app._userbeveiligingscode) < 4:
+                            app._userbeveiligingscode += pygame.key.name(event.key)[1:-1]
+                            print('Nog {} in te voeren.'.format(4 - len(app._userbeveiligingscode)))
+
+                        # Als de code gelijk is aan 4 cijfers, dan check je of de code juist is ingevoerd
+                        if len(app._userbeveiligingscode) == 4:
+                            if app._beveiligingscode == app._userbeveiligingscode or app._previousstatus == 'settings-new':
+                                if app._previousstatus == 'unarmed':
+                                    if app._deurdicht == True:
+                                        app.global_status = 'armed'
+                                    else:
+                                        app.global_status = app._previousstatus
+                                        print('De deur is nog open, deze dient eerst dichtgedaan te worden voordat het alarm kan worden ingesteld.')
+                                elif app._previousstatus == 'armed' or app._previousstatus == 'lockdown':
+                                    app.global_status = 'unarmed'
+                                    app._timer.cancel()
+                                    app._timer = Timer(3.0, app.start_anti_inbraak) # reset timer
+                                elif app._previousstatus == 'settings-check':
+                                    print('Voer een nieuwe beveiligingscode in:')
+                                    app.global_status = 'settings-new' # set previous state
+                                    app.global_status = 'inputcode'
+                                elif app._previousstatus == 'settings-new':
+                                    print('Gelukt, je oude code \033[32m{}\033[30m wordt veranderd in \033[32m{}\033[30m.'.format(app._beveiligingscode, app._userbeveiligingscode))
+                                    app._beveiligingscode = app._userbeveiligingscode
+                                    app.global_status = 'unarmed'
+                                # reset altijd de userbeveiligingscode
+                                app._userbeveiligingscode = ''
+                            else:
+                                print('De code is onjuist ingevoerd, de status wordt ingesteld naar {}'.format(app._previousstatus))
+                                if app._previousstatus == 'settings-check':
+                                    app.global_status = 'settings'
+                                else:
+                                    app.global_status = app._previousstatus
+                                # reset altijd de userbeveiligingscode
+                                app._userbeveiligingscode = ''
